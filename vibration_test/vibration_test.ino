d333af423c5fe851cde8984280c64c8d7b4e260c
Xiaoran0 <44989135+Xiaoran0@users.noreply.github.com>
2022-02-19 16:16:32 -0500

    added vibration disk test

---
+++ vibration_test/vibration_test.ino
@@ -0,0 +1,20 @@
+int vibNum = 5;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(vibNum, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  analogWrite(vibNum, 102);
+  delay(1000);                       // wait for a second
+  analogWrite(vibNum, 150);
+  delay(1000);
+  analogWrite(vibNum, 202);
+  delay(1000); 
+  analogWrite(vibNum, 0);
+  delay(1000); 
+}
